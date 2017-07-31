c4b871bce047c63f5e86d0940cf1d789856914d7
Rich Sharp <richpsharp@gmail.com>
2017-07-31 12:27:05 -0700

    marking 0.1.2 version numbers and pep256ing some code Branch: develop

--- taskgraph/__init__.py
+++ taskgraph/__init__.py
@@ -1,4 +1,5 @@
-__version__ = '0.1.1'
+"""taskgraph module."""
+from taskgraph.Task import TaskGraph
+from taskgraph.Task import Task
 
-from Task import TaskGraph
-from Task import Task
+__version__ = '0.1.2'
