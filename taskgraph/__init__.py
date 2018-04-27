02400b319c731e9416befdc004860310e0ab7262
James Douglass <jdouglass@stanford.edu>
2018-04-27 12:17:08 -0700

    Removing __future__ statements we don't use and using absolute_import.
    RE:#BITBUCKET-12
    Branch: feature/TASKGRAPH-12-python3

--- taskgraph/__init__.py
+++ taskgraph/__init__.py
@@ -1,10 +1,8 @@
 """taskgraph module."""
 from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
 from __future__ import absolute_import
 import pkg_resources
-from taskgraph.Task import *
+from .Task import *
 
 try:
     __version__ = pkg_resources.get_distribution(__name__).version
