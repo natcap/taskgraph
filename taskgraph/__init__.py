331f7c3a307cefaf3a0ec593ebf42e95eb75bdeb
James Douglass <jdouglass@stanford.edu>
2018-04-26 17:11:06 -0700

    Adding relevant __future__ imports.
    RE:#BITBUCKET-12
    Branch: feature/TASKGRAPH-12-python3

--- taskgraph/__init__.py
+++ taskgraph/__init__.py
@@ -1,4 +1,8 @@
 """taskgraph module."""
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
 import pkg_resources
 from taskgraph.Task import *
 
