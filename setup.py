1a9724c30f4bc7a08beb7316584fc54c4c71f19b
James Douglass <jdouglass@stanford.edu>
2019-02-20 13:10:29 -0800

    Adding python3.7 tag, tests.  RE:#BITBUCKET-51 Branch: feature/TESTING-51-implement-tests-on-pipelines

--- setup.py
+++ setup.py
@@ -31,5 +31,6 @@ setup(
         'Operating System :: POSIX',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: BSD License'
     ])
