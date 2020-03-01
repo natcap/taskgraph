744421f256e5dd96b18ef4c960955a2a20aa0016
Rich Sharp <richpsharp@gmail.com>
2020-02-29 17:20:46 -0800

    getting the __version__ into taskgraph again

--- taskgraph/__init__.py
+++ taskgraph/__init__.py
@@ -1,6 +1,9 @@
 """TaskGraph init module."""
+from pkg_resources import get_distribution
+
 from .Task import TaskGraph
 from .Task import Task
 from .Task import _TASKGRAPH_DATABASE_FILENAME
 
 __all__ = ['TaskGraph', 'Task', '_TASKGRAPH_DATABASE_FILENAME']
+__version__ = get_distribution(__name__).version
