f55bfcd46083745b34866875a22195a768782755
James Douglass <jdouglass@stanford.edu>
2023-10-27 11:23:50 -0700

    Trying out support for 3.12, dropping testing on 3.8.
    RE:#95

--- setup.py
+++ setup.py
@@ -34,10 +34,10 @@ setup(
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft',
         'Operating System :: POSIX',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.11',
         'License :: OSI Approved :: BSD License'
     ])
